4fe1f5f0871542c562aa85c0dc14cb1e82980db9
Trinh Dinh Viet <tdviet.dev@gmail.com>
2025-03-19 20:07:36 +0700

    fixing vulnerabilities

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,10 +3,7 @@ import babel from 'gulp-babel';
 import uglify from 'gulp-uglify';
 import rename from 'gulp-rename';
 
-gulp.task('default', () => {
-    gulp.src(['hermes.js', 'hermes-worker.js'])
-        .pipe(babel())
-        .pipe(uglify())
-        .pipe(rename(path => (path.extname = '.min.js')))
-        .pipe(gulp.dest('dist'));
-});
+gulp.task('default', () => gulp.src(['hermes.js', 'hermes-worker.js']).pipe(babel())
+    .pipe(uglify())
+    .pipe(rename(path => (path.extname = '.min.js')))
+    .pipe(gulp.dest('dist')));
